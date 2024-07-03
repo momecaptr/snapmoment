2b712df430c9a33fab4d8c005f164fd3a4096396
Siarhei Haurylchyk <sggavrilchik@gmail.com>
2024-07-04 01:18:12 +0300

    add ruls for eslint

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,6 @@
-import type { Preview } from "@storybook/react";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/700.css";
+import type { Preview } from '@storybook/react';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/700.css';
 
 const preview: Preview = {
   parameters: {
