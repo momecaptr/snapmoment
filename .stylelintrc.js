c9ac621ae4ebe9b3f7c0300f0129275da56d3ab3
momecaptr <momecap@proton.me>
2024-07-04 19:40:49 +0500

    merge dev to main (#19)
    * add storybook-deploy from development
    
    * Update README.md
    
    * Update README.md
    
    * refactor page content
    
    * refactor Dockerfile
    
    * Delete .idea directory
    
    * Linter (#12)
    
    * add ruls for eslint
    
    * add stylelint
    
    * finish work with Linters
    
    * fix
    
    * fix prettier format and eslint (#13)
    
    * add script for Minin stylelint (#14)
    
    * add script for Minin stylelint
    
    * fix
    
    * fix eslint prettier 0, add vercel.json
    
    * fix eslint prettier error
    
    ---------
    
    Co-authored-by: Daniil <daniilminin1990@yandex.ru>
    
    ---------
    
    Co-authored-by: Daniil <daniilminin1990@yandex.ru>
    Co-authored-by: Сергей <52825673+SiarheiHaurylchyk@users.noreply.github.com>

---
+++ .stylelintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-clean-order'],
+  rules: {
+    'custom-property-pattern': null,
+    'property-no-unknown': [
+      true,
+      {
+        ignoreProperties: ['composes'],
+      },
+    ],
+    'selector-class-pattern': null,
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global'],
+      },
+    ],
+  },
+};
