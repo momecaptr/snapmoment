e1eaad8259ae6c17cee47649647a2e86a0667473
momecaptr <momecap@proton.me>
2024-07-05 00:05:24 +0500

    Start point (#26)
    * text to prod
    
    * start development
    
    * start development
    
    ---------
    
    Co-authored-by: Daniil <daniilminin1990@yandex.ru>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import '@fontsource/roboto/700.css';
 export default function Home() {
   return (
     <main>
-      <h1 style={{ fontFamily: 'Roboto', color: 'green' }}>Production</h1>
+      <h1 style={{ fontFamily: 'Roboto', color: 'green' }}>DEVELOPMENT</h1>
     </main>
   );
 }
