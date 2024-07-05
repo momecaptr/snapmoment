691d92570834a3e1c225925d6b819cdd833c5c9d
Siarhei Haurylchyk <sggavrilchik@gmail.com>
2024-07-05 16:31:53 +0300

    add Input Button and Typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/700.css';
+import { Button } from '../components/button/Button';
+import Input from '@/components/Input/Input';
+import Typography from '@/components/Typography/Typography';
 export default function Home() {
   return (
     <main>
       <h1 style={{ fontFamily: 'Roboto', color: 'green' }}>DEVELOPMENT</h1>
+      <Button>123</Button>
+      <Input label={'123'} placeholder={'123'} />
+      <Typography as={'h1'} variant={'link2'}>
+        sdkjfks
+      </Typography>
     </main>
   );
 }
