62c84b20c6280b2429e59ed7a6a49dd766993e4f
Daniil <daniilminin1990@yandex.ru>
2024-07-07 00:34:49 +0500

    fix deployment. Master

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
 'use client';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/700.css';
-import { Button } from '@/components/ui/button/Button';
-import Input from '@/components/ui/Input/Input';
-import Typography from '@/components/ui/typography/Typography';
 export default function Home() {
   return (
     <main>
-      <h1 style={{ fontFamily: 'Roboto', color: 'green' }}>DEVELOPMENT</h1>
-      <Button>123</Button>
-      <Input label={'123'} placeholder={'123'} />
-      <Typography as={'h1'} variant={'link2'}>
-        sdkjfks
-      </Typography>
+      <h1 style={{ fontFamily: 'Roboto', color: 'green' }}>Production</h1>
     </main>
   );
 }
