403d694e7f15089684b531c620596696ce361172
Daniil <daniilminin1990@yandex.ru>
2024-07-07 22:23:01 +0500

    test husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/700.css';
 
 export default function Home() {
+  const res = 2;
+
   return (
     <main>
       <h1 style={{ color: 'green', fontFamily: 'Roboto' }}>DEVELOPMENT</h1>
