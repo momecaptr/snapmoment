9330fb56f09aa6c1cde1b11a88acbf850183f3a2
Daniil <daniilminin1990@yandex.ru>
2024-07-07 09:01:31 +0500

    refactor Input path

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,5 @@
 export * from './button/Button';
-export * from './input/Input';
+export { Input } from './input/Input';
 export * from './pagination/PaginationWithSelect';
 export * from './privacy/Privacy';
 export * from './select/Select';
