55405d0f935c0827b252807e7fbda78a2918c636
AndreyShal <andreyshalomitsky@gmail.com>
2024-07-13 00:19:20 -0700

    SNAP-21: feat add CreateNewPassword v2

--- src/lib/schemas/createNewPassword.schema.ts
+++ src/lib/schemas/createNewPassword.schema.ts
@@ -5,15 +5,15 @@ export const createNewPasswordSchema = z
     confirmPassword: z.string(),
     password: z
       .string()
-      .min(8, 'Пароль должен содержать не менее 8 символов')
-      .max(50, 'Пароль должен содержать не более 50 символов')
-      .regex(/[A-Z]/, 'Пароль должен содержать хотя бы одну заглавную букву')
-      .regex(/[a-z]/, 'Пароль должен содержать хотя бы одну строчную букву')
-      .regex(/[0-9]/, 'Пароль должен содержать хотя бы одну цифру')
-      .regex(/[^A-Za-z0-9]/, 'Пароль должен содержать хотя бы один специальный символ')
+      .min(8, { message: 'Password must be at least 8 characters long' })
+      .max(100, { message: 'Password must be at most 100 characters long' })
+      .regex(/[A-Z]/, { message: 'Password must contain at least one uppercase letter' })
+      .regex(/[a-z]/, { message: 'Password must contain at least one lowercase letter' })
+      .regex(/[0-9]/, { message: 'Password must contain at least one number' })
+      .regex(/[^A-Za-z0-9]/, { message: 'Password must contain at least one special character' })
   })
   .refine((data) => data.password === data.confirmPassword, {
-    message: 'Пароли не совпадают',
+    message: 'Password mismatch',
     path: ['confirmPassword']
   });
 
