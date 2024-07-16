9a92099a76dc0a811856331548d3c3113a54c760
BladeDancer <69kir969@mail.ru>
2024-07-17 01:04:57 +0300

    minor changes

--- src/shared/modal/Modal.tsx
+++ src/shared/modal/Modal.tsx
@@ -22,7 +22,7 @@ export const Modal = ({ children, className, style, title, ...props }: Props) =>
       <Dialog.Overlay className={s.DialogOverlay} />
       <Dialog.Content className={clsx(s.DialogContent)}>
         <Card className={clsx(className, s.card)} style={style}>
-          <div className={s.header}>
+          <div className={s.header} data-header={'header'}>
             <Typography as={'h1'} variant={'h1'}>
               {title}
             </Typography>
@@ -32,7 +32,9 @@ export const Modal = ({ children, className, style, title, ...props }: Props) =>
               </button>
             </Dialog.Close>
           </div>
-          <div className={s.content}>{children}</div>
+          <div className={s.content} data-content={'content'}>
+            {children}
+          </div>
         </Card>
       </Dialog.Content>
     </Dialog.Portal>
