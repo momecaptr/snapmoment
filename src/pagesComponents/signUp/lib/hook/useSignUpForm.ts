d1e358bcebbe3fa085af324635bebc87c0293bcd
Kirill <77087981+KirillDolzhenkov@users.noreply.github.com>
2024-07-16 14:43:39 +0300

    Sign-up + refactoring auth компонент (#76)
    * create useAutoId hook
    
    * created sign-up form + auth schema + auth forms refactoring
    
    * typography regular link color fix
    
    * changed project structure
    
    * fix imports

---
+++ src/pagesComponents/signUp/lib/hook/useSignUpForm.ts
@@ -0,0 +1,27 @@
+import { useForm } from 'react-hook-form';
+
+import { SignUpSchemaType, signUpSchema } from '@/features/schemas/authSchema/authShema';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const useSignUpForm = () => {
+  const {
+    control,
+    formState: { errors, isValid },
+    handleSubmit
+  } = useForm<SignUpSchemaType>({
+    /*mode: 'onBlur',*/
+    resolver: zodResolver(signUpSchema)
+  });
+
+  const onSubmit = (data: SignUpSchemaType) => console.log(data);
+
+  return {
+    control,
+    errors,
+    handleSubmit,
+    isValid,
+    onSubmit
+  };
+};
+
+export default useSignUpForm;
