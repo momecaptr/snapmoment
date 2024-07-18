0f2adcfa100bf1cb745146b9e2f9a38a942bafb5
ANDREI SHALOMITSKII <andreyshalomitsky@gmail.com>
2024-07-18 22:18:42 +0500

    SNAP-44: fix close DatePickerPopup (#82)
    * SNAP-44: fix close DatePickerPopup
    
    * SNAP-44: fix delete comment in DatePickerPopup
    
    * SNAP-44: fix delete comment in DatePickerPopup

--- src/shared/ui/datePicker/lib/hooks/useShowPopup.ts
+++ src/shared/ui/datePicker/lib/hooks/useShowPopup.ts
@@ -1,19 +1,30 @@
 'use client';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export const useShowPopup = () => {
   const elementRef = useRef<HTMLDivElement>(null);
   const [showPopup, setShowPopup] = useState(false);
-  const handleDocumentClick = (event: MouseEvent) => {
-    if (elementRef.current && !elementRef.current.contains(event.target as Node)) {
-      setShowPopup(false);
-      document.removeEventListener('mousedown', handleDocumentClick);
+
+  useEffect(() => {
+    const handleDocumentClick = (event: MouseEvent) => {
+      if (elementRef.current && !elementRef.current.contains(event.target as Node)) {
+        setShowPopup(false);
+      }
+    };
+
+    if (showPopup) {
+      document.addEventListener('click', handleDocumentClick);
+    } else {
+      document.removeEventListener('click', handleDocumentClick);
     }
-  };
+
+    return () => {
+      document.removeEventListener('click', handleDocumentClick);
+    };
+  }, [showPopup]);
 
   const handleInputClick = () => {
     setShowPopup(true);
-    document.addEventListener('mousedown', handleDocumentClick);
   };
 
   return {
