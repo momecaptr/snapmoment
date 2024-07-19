efdb2bfab8bc3a29f25f496740297967444c32de
Daniil <daniilminin1990@yandex.ru>
2024-07-19 21:54:03 +0500

    we need to remake project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,16 @@
 import type { AppProps } from 'next/app';
 
-import React, { ReactElement, ReactNode } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 
 import store from '@/app/store';
 import { Alert } from '@/entities';
 import { PageLayout } from '@/widget';
-import { NextPage } from 'next';
 
 import '../src/app/styles/index.scss';
 
-export type NextPageWithLayout<P = {}, IP = P> = {
-  getLayout?: (page: ReactElement) => ReactNode;
-} & NextPage<P, IP>;
-
-type AppPropsWithLayout = {
-  Component: NextPageWithLayout;
-} & AppProps;
-
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page);
-
-  return getLayout(
+export default function App({ Component, pageProps }: AppProps) {
+  return (
     <Provider store={store}>
       <PageLayout>
         <Component {...pageProps} />
--- src/widget/pageLayout/PageLayout.tsx
+++ src/widget/pageLayout/PageLayout.tsx
@@ -1,11 +1,14 @@
-import React, { PropsWithChildren, ReactElement } from 'react';
+import React, { ReactNode } from 'react';
 
 import { Header, SideBar } from '@/widget';
-import { NextPage } from 'next';
 
 import s from './PageLayout.module.scss';
 
-export const PageLayout: NextPage<PropsWithChildren> = (props) => {
+type Props = {
+  children: ReactNode;
+};
+
+export const PageLayout = (props: Props) => {
   const { children } = props;
 
   return (
@@ -19,8 +22,4 @@ export const PageLayout: NextPage<PropsWithChildren> = (props) => {
       <div className={s.content}>{children}</div>
     </main>
   );
-};
-
-export const getLayout = (page: ReactElement) => {
-  return <PageLayout>{page}</PageLayout>;
 };
