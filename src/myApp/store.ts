b5bbcdfda426c0f7a563a5cc41ecfcc1cd56e8aa
ANDREI SHALOMITSKII <andreyshalomitsky@gmail.com>
2024-07-22 00:30:46 +0500

    fix redux store on page router (#87)

--- src/myApp/store.ts
+++ src/myApp/store.ts
@@ -1,18 +1,17 @@
 import { alertSlice } from '@/entities/alert/model/alertSlice';
 import { appSlice } from '@/myApp/model/appSlice';
 import { configureStore } from '@reduxjs/toolkit';
+import { createWrapper } from 'next-redux-wrapper';
 
-export const makeStore = () => {
-  return configureStore({
+const makeStore = () =>
+  configureStore({
     reducer: {
       alertSlice,
       app: appSlice.reducer
     }
   });
-};
 
-// Infer the type of makeStore
 export type AppStore = ReturnType<typeof makeStore>;
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<AppStore['getState']>;
 export type AppDispatch = AppStore['dispatch'];
+export const wrapper = createWrapper<AppStore>(makeStore);
