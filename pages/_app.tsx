e8d02638ad9c301d268e3b91498f6b9bafa903ab
Daniil <daniilminin1990@yandex.ru>
2024-07-27 18:46:34 +0500

    add public posts qury and Posts component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,14 @@ import { Alert } from '@/entities';
 import StoreProvider from '@/myApp/StoreProvider';
 import { PageLayout } from '@/widget';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en';
+import ru from 'javascript-time-ago/locale/ru';
 
 import '@/myApp/styles/index.scss';
 
+TimeAgo.addDefaultLocale(en);
+TimeAgo.addLocale(ru);
 export default function App({ Component, pageProps }: AppProps) {
   const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '';
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-import { GeneralInfo } from '@/pagesComponents';
+import { Posts } from '@/pagesComponents/posts/Posts';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
 function Home() {
-  return <GeneralInfo />;
-  // return <MyToast />;
-  // return <SignUp />;
+  // return <GeneralInfo />;
+  return <Posts />;
 }
 
 // Home.getLayout = getLayout;
