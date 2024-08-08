f3299d912847420891695709783ea5fed7a283a6
Siarhei Haurylchyk <sggavrilchik@gmail.com>
2024-08-08 22:04:24 +0300

    fix coment

--- src/widget/index.ts
+++ src/widget/index.ts
@@ -1,22 +1,3 @@
-// export * from './generalInfoForms/GeneralInfoForms';
-// export * from './headSignInAndSignUp/HeadSignInAndSignUp';
-// export * from './header/Header';
-// export * from './modals';
-// export * from './postInteractionBar/PostInteractionBar';
-// export * from './sideBar/SideBar';
-// export * from './userCard/UserCard';
-// export * from '@/widget/generalInformation/accountManagement/AccountManagement';
-// export * from '@/widget/generalInformation/devices/Devices';
-// export * from '@/widget/generalInformation/generalInfoForms/GeneralInfoForms';
-// export * from '@/widget/generalInformation/myPayments/MyPayments';
-// export * from '@/widget/headSignInAndSignUp/HeadSignInAndSignUp';
-// export * from '@/widget/header/Header';
-// export * from '@/widget/modals';
-//
-// export * from '@/widget/postInteractionBar/PostInteractionBar';
-// export * from '@/widget/sideBar/SideBar';
-// export * from '@/widget/userCard/UserCard';
-
 export * from './headSignInAndSignUp/HeadSignInAndSignUp';
 export * from './header/Header';
 export * from './modals';
