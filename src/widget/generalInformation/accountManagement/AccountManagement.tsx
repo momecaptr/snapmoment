8242be7f9f152eea0c6d23fafb1743be1481090e
Daniil <daniilminin1990@yandex.ru>
2024-11-01 23:56:43 +0500

    add all modals

--- src/widget/generalInformation/accountManagement/AccountManagement.tsx
+++ src/widget/generalInformation/accountManagement/AccountManagement.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useGetCurrentPaymentSubscriptionQuery, useSendPaymentMutation } from '@/shared/api/device/paymentApi';
+import { ModalKey, useModal } from '@/shared/lib';
 import { Radio, Typography } from '@/shared/ui';
 import RenewalAndSubscriptionMenu from '@/widget/generalInformation/accountManagement/RenewalAndSubscriptionMenu';
 import PayPal from '@/widget/generalInformation/accountManagement/icon/PayPal';
 import Stripe from '@/widget/generalInformation/accountManagement/icon/Stripe';
+import { PaymentModals } from '@/widget/modals/paymentModals/PaymentModals';
+import { PaymentProceedModal } from '@/widget/modals/paymentProceedModal/PaymentProceedModal';
 import { clsx } from 'clsx';
 import { useRouter } from 'next/router';
 
@@ -25,14 +28,45 @@ export const subscriptionVariants = {
 // export type ChoosePaymentType = 'DAY' | 'MONTHLY' | 'WEEKLY';
 export type SubscriptionVariantTypes = (typeof subscriptionVariants)[keyof typeof subscriptionVariants];
 
-export const AccountManagement: React.FC = () => {
+const errorPayModalContentVariant = {
+  buttonText: 'Back to payment',
+  description: 'Transaction failed. Please, write to support.',
+  title: 'Error'
+};
+
+const successPayModalContentVariant = {
+  buttonText: 'OK',
+  description: 'Payment was successful!',
+  title: 'Success'
+};
+
+const chooseModalContentVariant = {
+  buttonText: 'Back to payment',
+  description: 'Please, choose payment type before proceed.',
+  title: 'Choose subscription plan'
+};
+
+export type PaymentModalContentType = typeof errorPayModalContentVariant;
+
+export const AccountManagement = () => {
   const { data } = useGetCurrentPaymentSubscriptionQuery();
+  // const {data: submissionResponse} = useGetSubmissionResponse()
   const [sendPayment] = useSendPaymentMutation();
-  const [business, setBusiness] = React.useState<boolean>(false);
-  const [savePaymentUrl, setSavePaymentUrl] = React.useState<string | undefined>(undefined);
-  const [choosePayment, setChoosePayment] = React.useState<SubscriptionVariantTypes | undefined>(undefined);
-  const [isPayment, setIsPayment] = React.useState<boolean>(false);
+  const [business, setBusiness] = useState<boolean>(false);
+  const [savePaymentUrl, setSavePaymentUrl] = useState<string | undefined>(undefined);
+  const [choosePayment, setChoosePayment] = useState<SubscriptionVariantTypes | undefined>(undefined);
+  const [isPayment, setIsPayment] = useState<boolean>(false);
+  const { isOpen: isPaymentModalOpen, setOpen: setIsPaymentModalOpen } = useModal(ModalKey.PaymentModals);
+  const { isOpen: isPaymentProceedOpen, setOpen: setIsPaymentProceedOpen } = useModal(ModalKey.PaymentProceed);
+  const [proceedStatus, setProceedStatus] = useState<boolean>(false);
+  const [paymentModalsContent, setPaymentModalsContent] = useState<PaymentModalContentType | undefined>(undefined);
   const router = useRouter();
+
+  // useEffect(() => {
+  //   if (submissionResponse) {
+  //     setBusiness(submissionResponse === 'business');
+  //   }
+  // }, [submissionResponse]);
 
   useEffect(() => {
     if (data?.data) {
@@ -51,16 +85,38 @@ export const AccountManagement: React.FC = () => {
     setChoosePayment(undefined);
   };
 
-  const handleSubmitPayment = async (paymentType: PaymentVariantTypes) => {
+  const submitPayment = async (paymentType: PaymentVariantTypes) => {
     if (!choosePayment) {
-      console.error('Please, choose payment type before proceed.');
+      // console.error('Please, choose subscription variant before proceed.');
+      setPaymentModalsContent(chooseModalContentVariant);
+      setIsPaymentModalOpen(true);
 
       return;
     }
 
+    // Открываем модалку для подтверждения платежа
+    setIsPaymentProceedOpen(true);
+    const proceed = await new Promise<boolean>((resolve) => {
+      setProceedStatus((status) => {
+        resolve(status);
+
+        return status;
+      });
+    });
+
+    // Если пользователь не подтвердил платеж, то выходим нахрен и не делаем запрос
+    if (!proceed) {
+      return;
+    }
+
     try {
+      setIsPaymentProceedOpen(true);
+      if (!proceedStatus) {
+        return;
+      }
       const response = await sendPayment({
         amount: 0,
+        // baseUrl: 'http://localhost:3000/profile/generalinfo',
         baseUrl: 'http://localhost:3000/profile/generalinfo',
         paymentType,
         typeSubscription: choosePayment
@@ -69,62 +125,103 @@ export const AccountManagement: React.FC = () => {
 
       setSavePaymentUrl(url);
       if (savePaymentUrl) {
+        setChoosePayment(undefined);
         await router.push(savePaymentUrl);
       }
     } catch (error) {
-      console.error(`Transaction failed. Please, write to support. \n ${paymentType}: ${error}`);
+      setPaymentModalsContent(errorPayModalContentVariant);
+      setIsPaymentModalOpen(true);
     }
   };
 
-  const handleSubmitPayPal = () => handleSubmitPayment(paymentVariants.paypal);
-  const handleSubmitStripe = () => handleSubmitPayment(paymentVariants.stripe);
+  const handleSubmitPaymentType = (e: React.MouseEvent<HTMLDivElement>) => {
+    const paymentType = e.currentTarget.dataset.payment as PaymentVariantTypes;
+
+    if (paymentType) {
+      void submitPayment(paymentType);
+    }
+  };
+
+  const handlePickSubscriptionType = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const subscriptionType = e.currentTarget.dataset.sub as SubscriptionVariantTypes;
+
+    subscriptionType && setChoosePayment(subscriptionType);
+  };
 
   return (
-    <div className={s.wrapperPayment}>
-      {!isPayment ? (
-        <div className={s.box}>
-          <Radio.Root className={s.radioRoot} name={'grade'}>
-            <Radio.Item onClick={handleSwitchPersonal} value={'1 radio'}>
-              Personal
-            </Radio.Item>
-            <Radio.Item onClick={handleOpenBusinessMenu} value={'2 radio'}>
-              Business
-            </Radio.Item>
-          </Radio.Root>
-        </div>
-      ) : (
-        <RenewalAndSubscriptionMenu />
-      )}
-
-      {business && (
-        <>
-          <Typography className={s.BusinessText} variant={'regular_text_16'}>
-            Your subscription costs:
-          </Typography>
-          <div className={s.boxBusiness}>
+    <>
+      <PaymentModals
+        content={paymentModalsContent}
+        openModal={isPaymentModalOpen}
+        setOpenModal={setIsPaymentModalOpen}
+      />
+      <PaymentProceedModal
+        openModal={isPaymentProceedOpen}
+        setOpenModal={setIsPaymentProceedOpen}
+        setProceedStatus={setProceedStatus}
+      />
+      <div className={s.wrapperPayment}>
+        {!isPayment ? (
+          <div className={s.box}>
             <Radio.Root className={s.radioRoot} name={'grade'}>
-              <Radio.Item onClick={() => setChoosePayment(subscriptionVariants.day)} value={'1 radio'}>
-                $10 per 1 day
+              <Radio.Item checked={!business} onClick={handleSwitchPersonal} value={'1 radio'}>
+                Personal
               </Radio.Item>
-              <Radio.Item onClick={() => setChoosePayment(subscriptionVariants.weekly)} value={'2 radio'}>
-                $50 per 7 days
-              </Radio.Item>
-              <Radio.Item onClick={() => setChoosePayment(subscriptionVariants.monthly)} value={'3 radio'}>
-                $100 per month
+              <Radio.Item checked={business} onClick={handleOpenBusinessMenu} value={'2 radio'}>
+                Business
               </Radio.Item>
             </Radio.Root>
           </div>
-          <div className={s.payIconBox}>
-            <div className={clsx(s.paymentTypeBox, s.boxPaypal)} onClick={handleSubmitPayPal}>
-              <PayPal />
+        ) : (
+          <RenewalAndSubscriptionMenu />
+        )}
+
+        {business && (
+          <>
+            <Typography className={s.BusinessText} variant={'regular_text_16'}>
+              Your subscription costs:
+            </Typography>
+            <div className={s.boxBusiness}>
+              <Radio.Root className={s.radioRoot} name={'grade'}>
+                <Radio.Item data-sub={subscriptionVariants.day} onClick={handlePickSubscriptionType} value={'1 radio'}>
+                  $10 per 1 day
+                </Radio.Item>
+                <Radio.Item
+                  data-sub={subscriptionVariants.weekly}
+                  onClick={handlePickSubscriptionType}
+                  value={'2 radio'}
+                >
+                  $50 per 7 days
+                </Radio.Item>
+                <Radio.Item
+                  data-sub={subscriptionVariants.monthly}
+                  onClick={handlePickSubscriptionType}
+                  value={'3 radio'}
+                >
+                  $100 per month
+                </Radio.Item>
+              </Radio.Root>
             </div>
-            <div>Or</div>
-            <div className={clsx(s.paymentTypeBox, s.boxStripe)} onClick={handleSubmitStripe}>
-              <Stripe />
+            <div className={s.payIconBox}>
+              <div
+                className={clsx(s.paymentTypeBox, s.boxPaypal)}
+                data-payment={paymentVariants.paypal}
+                onClick={handleSubmitPaymentType}
+              >
+                <PayPal />
+              </div>
+              <div>Or</div>
+              <div
+                className={clsx(s.paymentTypeBox, s.boxStripe)}
+                data-payment={paymentVariants.stripe}
+                onClick={handleSubmitPaymentType}
+              >
+                <Stripe />
+              </div>
             </div>
-          </div>
-        </>
-      )}
-    </div>
+          </>
+        )}
+      </div>
+    </>
   );
 };
