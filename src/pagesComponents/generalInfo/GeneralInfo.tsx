3fe199e97c442e2a22978cf2e7a4bf50639a44ec
KirillDolzhenkov <k.dolzhenkov@gmail.com>
2024-11-06 20:37:53 +0300

    feat: modified notices style & clearAll  button

--- src/pagesComponents/generalInfo/GeneralInfo.tsx
+++ src/pagesComponents/generalInfo/GeneralInfo.tsx
@@ -4,15 +4,10 @@ import { GeneralInfoNavigation } from '@/features';
 import { ModalKey, useModal } from '@/shared/lib';
 import { Wrapper } from '@/shared/ui';
 import { AccountManagement, AddProfilePhotoModal, Devices, GeneralInfoForms, MyPayments } from '@/widget';
-// import { AccountManagement, Devices, GeneralInfoForms, MyPayments } from '@/widget';
-// import { AddProfilePhotoModal } from '@/widgetaddProfilePhotoModal/AddProfilePhotoModal';
-
-import { useMeQuery } from '@/shared/api/auth/authApi';
 
 import s from './GeneralInfo.module.scss';
 
 export const GeneralInfo = () => {
-  const me = useMeQuery();
   const { isOpen, setOpen } = useModal(ModalKey.ChangePhoto);
   const [activeSection, setActiveSection] = useState('General information');
 
--- src/shared/ui/notificationItem/NotificationItem.tsx
+++ src/shared/ui/notificationItem/NotificationItem.tsx
@@ -1,22 +1,36 @@
-import { Typography } from '@/shared/ui';
+import { useState } from 'react';
+
+import Close from '@/../public/assets/components/Close';
+import { Button, Typography } from '@/shared/ui';
 
 import s from './NotificationItem.module.scss';
 
 type Props = {
+  id: number;
+  isRead: boolean;
   message: string;
 };
 
 export const NotificationItem = (props: Props) => {
   const { message } = props;
+  const [isRead, setIsRead] = useState();
+
+  const handleClick = () => {};
 
   return (
     <div className={s.notification}>
-      <div className={s.newBox}>
-        <Typography variant={'bold_text_16'}>New notice!</Typography>
-        <Typography className={s.new} variant={'small_text'}>
-          New
-        </Typography>
+      <div className={s.title}>
+        <div className={s.newBox}>
+          <Typography variant={'bold_text_16'}>New notice!</Typography>
+          <Typography className={s.new} variant={'small_text'}>
+            New
+          </Typography>
+        </div>
+        <Button className={s.closeButton} onClick={handleClick} title={'Clear'} variant={'text'}>
+          <Close />
+        </Button>
       </div>
+
       <Typography className={s.message} variant={'regular_text_14'}>
         {message}
       </Typography>
